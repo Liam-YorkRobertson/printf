0c3c6ea31ce3162c6557dc7c2476e9c1ed376e5c
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-03-28 10:27:52 -0700

    Test

--- get_precision.c
+++ get_precision.c
@@ -5,39 +5,39 @@
  * decimal places displayed
  *
  * @format: string in which arguments are printed
- * @n: list of arguments to be printed
+ * @i: list of arguments to be printed
  * @list: arguments list
  *
  * Return: precision for printing
  */
 
-int get_precision(const char *format, int *n, va_list list)
+int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_n = *n + 1;
+	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[curr_n] != '.')
+	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
 
-	for (curr_n += 1; format[curr_n] != '\0'; curr_n++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[curr_n]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
-			precision += format[curr_n] - '0';
+			precision += format[curr_i] - '0';
 		}
-		else if (format[curr_n] == '*')
+		else if (format[curr_i] == '*')
 		{
-			curr_n++;
+			curr_i++;
 			precision = va_arg(list, int);
 			break;
 		}
 		else
 			break;
 	}
-	*n = curr_n - 1;
+	*i = curr_i - 1;
 
 	return (precision);
 }
